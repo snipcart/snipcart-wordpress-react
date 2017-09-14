1a84147c521b17f538c8267d4363e7faa0ee39f8
maxlab <maxime@snipcart.com>
2017-09-14 09:40:33 -0400

    Fixed API key

--- src/App.js
+++ src/App.js
@@ -16,8 +16,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>A React & Snipcart powered e-commerce!</h2>
         </div>
         
         <div className="products">
